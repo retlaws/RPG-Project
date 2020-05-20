0728bf8296b643bf77786d697158d231d639a0a5
retlaws <50522456+retlaws@users.noreply.github.com>
2020-05-20 21:41:26 +0100

    First commit

---
+++ RPG Project/Assets/Script/Combat/Fighter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
+        
+        Transform target;
+        Mover mover;
+        ActionScheduler actionScheduler;
+        Animator animator;
+
+        float timeSinceLastAttack = 0;
+
+        private void Awake()
+        {
+            mover = GetComponent<Mover>();
+            actionScheduler = GetComponent<ActionScheduler>();
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            timeSinceLastAttack += Time.deltaTime;
+
+            MoveToEnemyToAttack();
+        }
+
+        private void MoveToEnemyToAttack()
+        {
+            if (target == null) { return; }
+
+            if (target != null && GetDistanceToTarget() > weaponRange)
+            {
+                mover.MoveTo(target.position);
+            }
+            else
+            {
+                mover.Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if(timeSinceLastAttack < timeBetweenAttacks) { return; }
+            animator.SetTrigger("attack"); //This will trigger the Hit() event.
+            timeSinceLastAttack = 0;
+        }
+
+        void Hit() //Animation Event
+        {
+            target.GetComponent<Health>().TakeDamage(weaponDamage);
+        }
+
+        private float GetDistanceToTarget()
+        {
+            return Vector3.Distance(transform.position, target.position);
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            actionScheduler.StartAction(this);
+            target = combatTarget.transform;
+
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+    }
+}
+
---
+++ RPG Project/Assets/Script/Movement/Mover.cs
@@ -0,0 +1,55 @@
+﻿using RPG.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        NavMeshAgent navMeshAgent;
+        Animator animator;
+        ActionScheduler actionScheduler;
+
+        void Awake()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+            actionScheduler = GetComponent<ActionScheduler>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            actionScheduler.StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.isStopped = false;
+            navMeshAgent.destination = destination;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            animator.SetFloat("forwardSpeed", speed);
+        }
+    }
+}
+
